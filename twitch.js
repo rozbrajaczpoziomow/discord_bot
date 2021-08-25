d3c1298b61a2a8f29f1608f7ce2a9f62cd1147ef
rozbrajaczpoziomow <rozbrajaczpoziomow@gmail.com>
2021-08-25 18:53:14 +0200

    i'm dumb

--- twitch.js
+++ twitch.js
@@ -8,7 +8,7 @@ module.exports = async main => {
 		const token = json.access_token;
 		res(
 			setInterval(async () => {
-				const stream = await fetch('https://api.twitch.tv/helix/streams?user_login=tyrolkagd&first=1', { headers: { 'Client-Id': twitch.id, Authorization: `Bearer ${token}` } });
+				const stream = await fetch('https://api.twitch.tv/helix/streams?user_login=saltaxatlas&first=1', { headers: { 'Client-Id': twitch.id, Authorization: `Bearer ${token}` } });
 				const stream_json = await stream.json();
 				// console.log(stream, stream_json)
 				const is_streaming = Object.keys(stream_json.data).length !== 0;
