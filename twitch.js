a404276c45e768046fcdd4fa946e6488c29e3e0a
rozbrajaczpoziomow <rozbrajaczpoziomow@gmail.com>
2021-08-26 18:43:59 +0200

    try catch if fetch to twitch api doesn't work

--- twitch.js
+++ twitch.js
@@ -8,7 +8,13 @@ module.exports = async main => {
 		const token = json.access_token;
 		res(
 			setInterval(async () => {
-				const stream = await fetch('https://api.twitch.tv/helix/streams?user_login=saltaxatlas&first=1', { headers: { 'Client-Id': twitch.id, Authorization: `Bearer ${token}` } });
+				try {
+					const stream = await fetch('https://api.twitch.tv/helix/streams?user_login=saltaxatlas&first=1', { headers: { 'Client-Id': twitch.id, Authorization: `Bearer ${token}` } });
+				} catch(e) {
+					console.log('\t[!] Fetch Error! Stack trace:')
+					console.log(e.stack);
+					return;
+				}
 				const stream_json = await stream.json();
 				// console.log(stream, stream_json)
 				const is_streaming = Object.keys(stream_json.data).length !== 0;
